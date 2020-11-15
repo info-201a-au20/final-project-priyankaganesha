ff7a0be0ba5f1ff5c6af2054f72475ebf9e33587
stlp <470meebarrow@gmail.com>
2020-11-15 11:45:18 -0800

    added an r file for obesity data

---
+++ Obesity_data.R
@@ -0,0 +1,4 @@
+obesity <- read.csv("/Users/stlp/Github/Informatics201/group-project/final-project-priyankaganesha/data/obesity-cleaned.csv",
+                    stringsAsFactors = FALSE)
+nrow(obesity)
+ncol(obesity)
