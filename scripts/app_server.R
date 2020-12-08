6c8e8a10f7390d7a1c014a519804a4bc401335c6
unknown <megant33@uw.edu>
2020-12-08 14:44:50 -0800

    Interactive page 2 done?

--- scripts/app_server.R
+++ scripts/app_server.R
@@ -2,8 +2,79 @@
 library(shiny)
 library(ggplot2)
 library(plotly)
-library(dplR)
+library(dplyr)
+
+
 
 server <- function(input, output){
+  #loading data
+  df_depress <- read.csv("C:/Users/Megan/Documents/Junior Year UW/info201/final-project-priyankaganesha/data/Indicators_of_Anxiety_or_Depression_Based_on_Reported_Frequency_of_Symptoms_During_Last_7_Days.csv", stringsAsFactors = FALSE)
+  
+  #Page One Plots
+  
+  
+  #Page Two Plots
+  #First plot: National Percentages of Anxiety or Depression Symptoms Over Time
+  output$national_plot <- renderPlotly({
+    # sort the symptom
+    df_nation <- filter(df_depress, Indicator == input$symp_select,
+                        Group == "National Estimate",
+                        ï..Phase != -1)
+    
+    # Symptom names and title
+    symp_names <- c(
+      "Anxiety" = "Symptoms of Anxiety Disorder",
+      "Depression" = "Symptoms of Depressive Disorder",
+      "Anxiety or Depression" = "Symptoms of Anxiety Disorder or Depressive Disorder"
+    )
+    symp_title <- paste0(
+      "Indicators of ",
+      names(symp_names[which(symp_names == input$symp_select)]),
+      " in the US over time"
+    )
+    
+    # return the plot
+    national_plot <- ggplot(data = df_nation, aes(x = Time.Period.Label, 
+                                             y = Value, group = 1)) +
+      geom_line() +
+      geom_point() +
+      
+      labs(title = symp_title,
+           y = "Percent of People Reporting Symptoms",
+           x = "Time Period"
+      ) +
+      theme_classic() +
+      theme(axis.text.x = element_text(angle = -90, hjust = 0, vjust = 0))
+    
+    national_plot <- ggplotly(national_plot)
+    return(national_plot)
+  })
+  # Second Plot: Trends of New Daily Covid Cases over time
+  output$case_plot <- renderPlotly({
+    #added data
+    df_case <- filter(df_depress, Indicator == input$symp_select,
+                        Group == "National Estimate",
+                        ï..Phase != -1) %>%
+      mutate(cases = c(37163, 31099, 26995, 20448, 21361, 20548, 21860, 27957, 
+                     40632, 52823, 59273, 72315, 46796, 40178, 42163, 45515, 
+                     60238))
+    
+    # return the plot
+    case_plot <- ggplot(data = df_sort, aes(x = Time.Period.Label, y = cases, 
+                                            group = 1)) +
+      geom_point()+
+      geom_line()+
+      labs(title = "The Number of New COVID-19 Cases Over Time",
+           y = "Number Of New Cases",
+           x = "Time Period"
+      ) +
+      
+      theme_classic() +
+      theme(axis.text.x = element_text(angle = -90, hjust = 0, vjust = 0))
+    case_plot <- ggplotly(case_plot)
+    return(case_plot)
+  })
+  
+  #Page Three Plots
   
 }
--- scripts/app_ui.R
+++ scripts/app_ui.R
@@ -1,13 +1,29 @@
 # UI for project
-library(shiny)
-library(ggplot2)
-library(plotly)
+library("shiny")
+library("ggplot2")
+library("plotly")
 
 project_overview <- tabPanel(
   "Overview of our Project",
-  mainPanel(
-    "Enter overview here"
-  )
+
+    p("Since the pandemic hit the world, it has drastically turned the lives 
+    of humans all of the world upside down. One of many concerns that the 
+    COVID-19 pandemic as highlighted is that of mental health. Being under 
+    lockdown and limiting social interactions has led way for increased cases 
+    of anxiety and depression in people.The data we are analyzing was collected 
+    from a national household pulse survey
+    by the U.S. Census Bureau meant to produce data on the social and
+    economic impacts of Covid-19 on households in the US. This portion 
+    of the survey is on the indicators of anxiety or depression changing 
+    over time, during the pandemic, on different ages, races, education 
+    levels and states. We chose this dataset because it is very relevant for 
+    observing how many people are experiencing mental health issues during 
+    the pandemic. The questions we want to answer are: QUESTION ONE, 
+    Do Changes in Percentage of Mental Health Symptoms Correlate With 
+    Amounts of COVID-19 Cases Over Time?, QUESTION THREE"),
+    p("INSERT DATASET SOURCE"),
+    p("INSERT PICTURE OR SOME SORT OF VISUAL ELEMENT")
+  
 )
 
 page_one_widget <- sidebarPanel(
@@ -26,16 +42,51 @@ page_one <- tabPanel(
   )
 )
 
+#Here is Page Two
 page_two_widget <- sidebarPanel(
-  
+  radioButtons(
+    inputId = "symp_select",
+    label = ("Symptom Selection:"),
+    choices = c("Anxiety" = "Symptoms of Anxiety Disorder",
+    "Depression" = "Symptoms of Depressive Disorder",
+    "Anxiety or Depression" = "Symptoms of Anxiety Disorder or Depressive Disorder"
+    ),
+    selected = "Symptoms of Anxiety Disorder"
+  )  
 )
 
 page_two_main <- mainPanel(
+
+  plotlyOutput("national_plot"),
+  p("This first plot above was created to answer how the percent of 
+    people feeling depressed and/or anxious differs over time. 
+    The time period of this chart goes from the beginning to the 
+    end of the survey, from Apr 23 - May 5 to Oct 14 - Oct 26 of 2020. 
+    There are three groups that can be selected in this chart: people 
+    suffering from anxiety, people suffering from depression and 
+    people suffering from either."),
   
+  plotlyOutput("case_plot"),
+  p("This second plot above was created to show the number of new cases on the
+    first days of the Time Periods that the survey uses. 
+    (Apr 23 to Oct 14 of 2020.)"),
 )
 
 page_two <- tabPanel(
-  "Enter Question one here:",
+  "Mental Health Changes Relating to COVID-19 Cases",
+  
+  titlePanel("Do Changes in Percentage of Mental Health Symptoms Correlate With Amounts of COVID-19 Cases Over Time?"),
+  
+  p("The two plots below show trends for how the percent of people experiencing
+  mental health symptoms and COVID-19 cases changes over time. When looking at
+  the two graphs together they resemble a very similar graph with peaks and 
+  valleys at almost identical time points. The highest symptom percentages for 
+  all groups was in the time period from July 16 - July 21 2020, and the highest 
+  daily COVID-19 cases out of all the time periods was the same period. The 
+  trends following a similar trajectory, suggesting that anxiety and
+  depression could be triggered by the severity of daily cases reported in 
+  the same time periods in the United States."),
+  
   sidebarLayout(
     page_two_main,
     page_two_widget
@@ -62,9 +113,13 @@ page_three <- tabPanel(
 
 summary_page <- tabPanel(
   "Summary of our Project",
-  mainPanel(
-    "Enter summary here"
-  )
+
+#First Takeaway
+#Second Takeaway
+f4("Second Takeaway: Mental Health Relations With COVID-19 Cases"),
+p(""),
+#Third Takeaway
+  
 )
 
 ui <- navbarPage(
